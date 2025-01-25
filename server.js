aa16f8ca47b40a5b9198a6e3a8dfc56ad38e6ad1
Ashi-Jain07 <106330938+Ashi-Jain07@users.noreply.github.com>
2025-01-25 15:13:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,11 +2,6 @@ import express from 'express'
 import nodemailer from 'nodemailer'
 import cors from 'cors'
 import dotenv from 'dotenv';
-import passport from './config/passportConfig.js'; // Passport config file
-import session from 'express-session'; // Session to manage user login state
-import { userRoutes } from './Routes/user.routes.js';
-import authRoutes from './Routes/auth.routes.js';
-import mongoose from 'mongoose';
 
 // Load environment variables from .env file
 dotenv.config();
@@ -15,28 +10,6 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors());
-
-app.use(session({ secret: 'your_secret_key', resave: false, saveUninitialized: true }));
-
-// Initialize passport for handling authentication
-app.use(passport.initialize());
-app.use(passport.session());
-
-// Use the routes
-app.use(authRoutes);
-
-// MongoDB connection
-mongoose.connect("mongodb+srv://dev:Glidex%40123@cluster0.shf7f.mongodb.net/");
-mongoose.set('debug', true);
-const db = mongoose.connection;
-
-db.on("open", () => {
-    console.log("Connection Successful");
-});
-
-db.on("error", (error) => {
-    console.log("Connection not Successful", error);
-});
 
 // Nodemailer Configuration
 const transporter = nodemailer.createTransport({
@@ -125,5 +98,3 @@ app.post("/subscribe", async (req, res) => {
 const PORT = process.env.PORT || 3000;
 // Start the Server
 app.listen(PORT, () => console.log("Server running on http://localhost:3000"));
-
-userRoutes(app)
